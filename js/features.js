a00f6078911babfeca115464fcc5065ad1393600
Nur Uddin <nuruddinnur538@gmail.com>
2024-12-01 18:19:47 +0600

    first commit

---
+++ js/features.js
@@ -0,0 +1,13 @@
+document.getElementById('btn-show-cash-out')
+.addEventListener('click', function(){
+    console.log('show cash out button clicked')
+    document.getElementById('cash-out-form').classList.remove('hidden');
+    document.getElementById('add-money-form').classList.add('hidden');
+})
+
+document.getElementById('btn-show-add-money')
+.addEventListener('click', function (){
+    document.getElementById('add-money-form').classList.remove('hidden');
+    document.getElementById('cash-out-form').classList.add('hidden');
+
+})
